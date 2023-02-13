64182b767cc5aa51b761f51b15933dfd3658352b
Joel Kurien <Joel.Kurien@gep.com>
2023-02-13 15:19:02 +0530

    UsersAPI Added

---
+++ UsersAPI/Model/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersAPI.Model
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public sbyte isManager { get; set; }    
+    }
+}
